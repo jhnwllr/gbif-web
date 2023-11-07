3dc58c423a241fb43744d09ac2067c67d4752c67
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2023-11-07 17:49:46 +0100

    test

--- packages/graphql-api/src/api-utils/ip2country.ctrl.js
+++ packages/graphql-api/src/api-utils/ip2country.ctrl.js
@@ -13,16 +13,21 @@ router.get('/get-country', (req, res, next) => {
 
   // Check if the IP address is "localhost" or "127.0.0.1" and handle it
   if (clientIP === '::1' || clientIP === '127.0.0.1') {
-    return res.json({ country: null });
+    return res.json({ country: null, error: 'Localhost' });
   }
 
   // Get the country code for the client's IP address
   try {
     const countryCode = ip3country.lookupStr(clientIP);
+    const debugInfo = {
+      forwardedFor: req.header('x-forwarded-for'),
+      reqIp: req.ip,
+      remoteAddress: req.socket.remoteAddress
+    }
     if (countryCode) {
-      res.json({ country: countryCode, ip: clientIP });
+      res.json({ country: countryCode, ip: clientIP, ...debugInfo });
     } else {
-      res.json({ country: null, ip: clientIP });
+      res.json({ country: null, ip: clientIP, ...debugInfo, error: 'No country code found' });
     }
   } catch(err) {
     res.status(500).json({ country: null, error: 'Internal Server Error' });
