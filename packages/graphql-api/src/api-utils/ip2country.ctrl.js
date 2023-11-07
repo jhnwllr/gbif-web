fca51b2e0d1d738cec66efd8e2ea37c70692b444
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2023-11-07 20:47:28 +0100

    translation

--- packages/graphql-api/src/api-utils/ip2country.ctrl.js
+++ packages/graphql-api/src/api-utils/ip2country.ctrl.js
@@ -1,18 +1,20 @@
+import axios from 'axios';
 import { Router } from 'express';
 import ip3country from 'ip3country';
+import config from '../config';
+
 const router = Router();
 ip3country.init();
+const minute = 60;
+const translationEndpoint = config.translations || 'https://react-components.gbif.org/lib/translations';
 
 export default (app) => {
-  app.use('/unstable-api/ip', router);
+  app.use('/unstable-api', router);
 };
 
-router.get('/get-country', (req, res, next) => {
-  // const clientIP = req.ip;
+router.get('/user-info', async (req, res, next) => {
+  const lang = req.query.lang; // Extract the "lang" query parameter from the URL.
   const clientIP = req.header('x-forwarded-for') || req.socket.remoteAddress;
-
-  // res.setHeader('Cache-Control', 'private, max-age=0');
-  res.setHeader('Cache-Control', 'no-cache');
 
   // Check if the IP address is "localhost" or "127.0.0.1" and handle it
   if (clientIP === '::1' || clientIP === '127.0.0.1') {
@@ -27,12 +29,38 @@ router.get('/get-country', (req, res, next) => {
       reqIp: req.ip,
       remoteAddress: req.socket.remoteAddress
     }
+    res.setHeader('Cache-Control', 'private, max-age=' + 10 * minute);
     if (countryCode) {
-      res.json({ country: countryCode, ip: clientIP, ...debugInfo });
+      const translation = {};
+      if (lang) {
+        let countryName = await getCountryName(lang, countryCode);
+        if (!countryName) {
+          countryName = await getCountryName('en', countryCode);
+        }
+        if (!countryName) {
+          translation.error = 'Unable to find a translation for that language code and language'
+        } else {
+          translation.countryName = countryName;
+        }
+      }
+      res.json({ country: countryCode, ip: clientIP, ...debugInfo, translation });
     } else {
       res.json({ country: null, ip: clientIP, ...debugInfo, error: 'No country code found' });
     }
-  } catch(err) {
+  } catch (err) {
+    res.setHeader('Cache-Control', 'no-cache');
     res.status(500).json({ country: null, error: 'Internal Server Error' });
   }
 });
+
+// get translation for a given language code
+async function getCountryName(lang, countryCode) {
+  const apiUrl = `${translationEndpoint}/${lang}.json`;
+  try {
+    const response = await axios.get(apiUrl);
+    const countryName = response.data[`enums.countryCode.${countryCode}`];
+    return countryName;
+  } catch (error) {
+    return null;
+  }
+}
