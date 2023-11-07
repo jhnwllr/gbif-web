51416fb98d2b01e28c55a9fb32fb4d9770b3a458
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2023-11-07 17:43:59 +0100

    fix: get ip from req

--- packages/graphql-api/src/api-utils/ip2country.ctrl.js
+++ packages/graphql-api/src/api-utils/ip2country.ctrl.js
@@ -8,23 +8,23 @@ export default (app) => {
 };
 
 router.get('/get-country', (req, res, next) => {
-  const clientIP = req.ip;
+  // const clientIP = req.ip;
+  const clientIP = req.header('x-forwarded-for') || req.socket.remoteAddress;
 
   // Check if the IP address is "localhost" or "127.0.0.1" and handle it
   if (clientIP === '::1' || clientIP === '127.0.0.1') {
-    res.json({ country: 'unknown' });
-    return;
+    return res.json({ country: null });
   }
 
   // Get the country code for the client's IP address
   try {
     const countryCode = ip3country.lookupStr(clientIP);
     if (countryCode) {
-      res.json({ country: countryCode });
+      res.json({ country: countryCode, ip: clientIP });
     } else {
-      res.json({ country: 'unknown' });
+      res.json({ country: null, ip: clientIP });
     }
   } catch(err) {
-    res.status(500).json({ country: 'unknown', error: 'Internal Server Error' });
+    res.status(500).json({ country: null, error: 'Internal Server Error' });
   }
 });
